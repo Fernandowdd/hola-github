9c0114fe540397800d30e5f7b9471b40140a5cbd
Swank <fer123luis123@gmail.com>
2025-11-10 09:00:13 -0500

    Modificamos primera linea

--- gitpractica.py
+++ gitpractica.py
@@ -1,4 +1,4 @@
-print("Estoy practicando mi git")
+print("Estoy haciendo 2cmabios ahí")
 print("El diablo loco ajjaj")
 print("Estoy haciendo cambios efectivos en la segunda rama")
 print("Hoy de nuevo cerraste la puerta")
